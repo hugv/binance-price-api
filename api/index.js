99aba8e3918c03d37b59eb29f9b94a642823d8e8
hugv <67804014+hugv@users.noreply.github.com>
2025-07-16 12:47:07 -0500

    Delete api directory

--- api/index.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const symbol = req.query.symbol || 'BTCUSDT';
-  const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`);
-  const data = await response.json();
-  res.status(200).json({ price: data.price });
-}
