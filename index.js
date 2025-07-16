9d307c33c613c600a93d83181b05f8438fd59855
hugv <67804014+hugv@users.noreply.github.com>
2025-07-16 12:41:20 -0500

    Eliminar archivo index.js de la raíz

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-export default async function handler(req, res) {
-  const { symbol } = req.query;
-  const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (!data.price) {
-      return res.status(404).json({ error: "Símbolo no válido" });
-    }
-
-    res.status(200).json({ price: data.price });
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener el precio" });
-  }
-}
